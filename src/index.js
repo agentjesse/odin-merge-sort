571159cc0e69d756cfdcb325ce7697b6f4902970
jesse <31576321+agentjesse@users.noreply.github.com>
2024-03-30 20:12:15 -0400

    eslintrc mod for node imports extensions

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 /* Next task:
--  
+-  Build a function mergeSort that takes in an array and returns a sorted array, using a recursive merge sort methodology. An input of [3, 2, 1, 13, 8, 5, 0, 1] should return [0, 1, 1, 2, 3, 5, 8, 13], and an input of [105, 79, 100, 110] should return [79, 100, 105, 110].
 */
 //In Node.js, when importing local modules, it's necessary to include the file extension (e.g., .js) in the import statement. 
 import { logToConsole as lg, tableToConsole as tb } from './logger.js'; //shorthand loggers
 
+const mergeSort = arr=> {
+  
+};
